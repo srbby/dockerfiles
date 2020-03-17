0e81eca83cd8765240da098773d890c5aab6b8e4
Sergey Bobysh <serb@tut.by>
2020-03-17 22:18:39 +0300

    Prevented bat file comments from being printed during execution

--- youtube-dl/run.bat
+++ youtube-dl/run.bat
@@ -1,10 +1,10 @@
 docker build -t youtube-dl .
 
-REM Show available download options
-REM docker run --rm youtube-dl -F "youtube-link"
-REM Download file with selected option
-REM docker run --rm -v %CD%/download:/download youtube-dl -f 22 "youtube-link"
+:: Show available download options
+:: docker run --rm youtube-dl -F "youtube-link"
+:: Download file with selected option
+:: docker run --rm -v %CD%/download:/download youtube-dl -f 22 "youtube-link"
 
-REM docker run --rm -v %CD%/download:/download youtube-dl "youtube-link"
-REM docker run --rm -v %CD%/download:/download youtube-dl --extract-audio --audio-format mp3 "youtube-link"
-REM docker run --rm -v %CD%/download:/download youtube-dl -f bestaudio "youtube-link"
+:: docker run --rm -v %CD%/download:/download youtube-dl "youtube-link"
+:: docker run --rm -v %CD%/download:/download youtube-dl --extract-audio --audio-format mp3 "youtube-link"
+:: docker run --rm -v %CD%/download:/download youtube-dl -f bestaudio "youtube-link"
