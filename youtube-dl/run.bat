131312f17f88a92f99f76e08101de0d0e0d8a5fa
Sergey Bobysh <serb@tut.by>
2021-09-21 21:41:07 +0300

    Added ability to run multiple containers in parallel

--- youtube-dl/run.bat
+++ youtube-dl/run.bat
@@ -3,8 +3,8 @@ docker build -t youtube-dl .
 :: Show available download options
 :: docker run --rm youtube-dl -F "youtube-link"
 :: Download file with selected option
-:: docker run --rm -v %CD%/download:/download youtube-dl -f 22 "youtube-link"
+:: start /b docker run --rm -v %CD%/download:/download youtube-dl -f 22 "youtube-link"
 
-:: docker run --rm -v %CD%/download:/download youtube-dl "youtube-link"
-:: docker run --rm -v %CD%/download:/download youtube-dl --extract-audio --audio-format mp3 "youtube-link"
-:: docker run --rm -v %CD%/download:/download youtube-dl -f bestaudio "youtube-link"
+:: start /b docker run --rm -v %CD%/download:/download youtube-dl "youtube-link"
+:: start /b docker run --rm -v %CD%/download:/download youtube-dl --extract-audio --audio-format mp3 "youtube-link"
+:: start /b docker run --rm -v %CD%/download/test:/download youtube-dl -f bestaudio "youtube-link"
